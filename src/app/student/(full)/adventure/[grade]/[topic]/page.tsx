965531f21b3339d28541153f65ab3e7b43e0cec4
coldwind444 <minhtan14042004@gmail.com>
2025-10-13 20:26:57 +0700

    change structure of lesson

---
+++ src/app/student/(full)/adventure/[grade]/[topic]/page.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+interface MilestoneProps {
+    params: {
+        grade: string
+        topic: string
+    }
+}
+
+const Milestones : FC<MilestoneProps> = ({ params }) => {
+    return (
+        <div></div>
+    )
+}
+
+export default Milestones
